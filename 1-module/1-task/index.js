221a77bd18bbf7a32e31f2d107d741db9a11c8b2
Maksim Levanovich <119817136+MaxLevUS@users.noreply.github.com>
2024-02-19 21:12:38 -0600

    factorial of 5

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1;
+
+  for (let i = 2; i <= n; i++) {
+    result = result * i;
+    console.log(
+      `Зашли в цикл,i = ${i}. Умножаем i на возрастающий при каждой итерации result`
+    );
+  }
+  return result;
 }
+let result = factorial(5);
+console.log(`Факториал равен ${result}`);
