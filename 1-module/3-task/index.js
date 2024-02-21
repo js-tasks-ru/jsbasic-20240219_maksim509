6f9c5f8d4152e1391e788aff60236be1a5c98412
Maksim Levanovich <maxlevanovich@gmail.com>
2024-02-20 18:37:17 -0600

    поменял Петю на Васю в задании номер 3т

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -2,4 +2,6 @@ function ucFirst(str) {
   // ваш код...
   return str ? str[0].toUpperCase() + str.slice(1) : str;
 }
-console.log(ucFirst("петя"));
+console.log(ucFirst("вася"));
+console.log(ucFirst("в"));
+console.log(ucFirst(""));
