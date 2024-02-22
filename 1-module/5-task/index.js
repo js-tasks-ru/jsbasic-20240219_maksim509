1fc170b63d46a1b9dd34d7cc763aec9d73f78a03
Maksim Levanovich <maxlevanovich@gmail.com>
2024-02-22 17:53:22 -0600

    Решил задачу номер 5

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,11 @@
 function truncate(str, maxlength) {
   // ваш код...
+
+  if (str.length <= maxlength) {
+    return str;
+  } else {
+    return str.slice(0, maxlength - 1) + "…";
+  }
 }
+console.log(truncate("Вот, что мне хотелось бы сказать на эту тему:", 20));
+console.log(truncate("Всем привет!", 20));
