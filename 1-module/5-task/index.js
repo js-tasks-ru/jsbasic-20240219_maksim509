556c9084310f2d15dbb4baccf1d09ae68516bd3b
Maksim Levanovich <maxlevanovich@gmail.com>
2024-02-23 08:29:50 -0600

    Переделал условие на тернарный оператор, так функция стала короче

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,11 +1,12 @@
 function truncate(str, maxlength) {
   // ваш код...
+  return str.length <= maxlength ? str : str.slice(0, maxlength - 1) + "…";
 
-  if (str.length <= maxlength) {
-    return str;
-  } else {
-    return str.slice(0, maxlength - 1) + "…";
-  }
+  //   if (str.length <= maxlength) {
+  //     return str;
+  //   } else {
+  //     return str.slice(0, maxlength - 1) + "…";
+  //   }
 }
 truncate("Вот, что мне хотелось бы сказать на эту тему:", 20);
 truncate("Всем привет!", 20);
