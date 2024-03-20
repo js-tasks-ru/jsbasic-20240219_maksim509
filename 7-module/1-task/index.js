426085d3394efbb3e3c18836fc02cddaddd594d0
Maksim Levanovich <maxlevanovich@gmail.com>
2024-03-19 20:49:38 -0500

    7-module

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -1,7 +1,93 @@
-import createElement from '../../assets/lib/create-element.js';
+// import createElement from '../../assets/lib/create-element.js';
+
+// export default class RibbonMenu {
+//   constructor(categories) {
+//     this.categories = categories;
+//   }
+// }
+
+import createElement from "../../assets/lib/create-element.js";
 
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+    this.render();
+    this.addEventListeners();
+  }
+
+  render() {
+    const ribbon = createElement(`
+      <div class="ribbon">
+        <button class="ribbon__arrow ribbon__arrow_left">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </button>
+        <div class="ribbon__inner">
+          ${this.categories
+            .map(
+              (category) => `
+            <a href="#" class="ribbon__item" data-id="${category.id}">${category.name}</a>
+          `
+            )
+            .join("")}
+        </div>
+        <button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </button>
+      </div>
+    `);
+    this.elem = ribbon;
+  }
+
+  addEventListeners() {
+    const inner = this.elem.querySelector(".ribbon__inner");
+    const leftArrow = this.elem.querySelector(".ribbon__arrow_left");
+    const rightArrow = this.elem.querySelector(".ribbon__arrow_right");
+
+    inner.addEventListener("click", (event) => {
+      event.preventDefault();
+      const link = event.target.closest(".ribbon__item");
+      if (!link) return;
+
+      const currentActiveLink = inner.querySelector(".ribbon__item_active");
+      if (currentActiveLink) {
+        currentActiveLink.classList.remove("ribbon__item_active");
+      }
+
+      link.classList.add("ribbon__item_active");
+
+      const categoryId = link.dataset.id;
+      const ribbonSelectEvent = new CustomEvent("ribbon-select", {
+        detail: categoryId,
+        bubbles: true,
+      });
+      this.elem.dispatchEvent(ribbonSelectEvent);
+    });
+
+    inner.addEventListener("scroll", () => {
+      const scrollWidth = inner.scrollWidth;
+      const scrollLeft = inner.scrollLeft;
+      const clientWidth = inner.clientWidth;
+      const scrollRight = scrollWidth - scrollLeft - clientWidth;
+
+      if (scrollLeft === 0) {
+        leftArrow.classList.remove("ribbon__arrow_visible");
+      } else {
+        leftArrow.classList.add("ribbon__arrow_visible");
+      }
+
+      if (scrollRight < 1) {
+        rightArrow.classList.remove("ribbon__arrow_visible");
+      } else {
+        rightArrow.classList.add("ribbon__arrow_visible");
+      }
+    });
+
+    leftArrow.addEventListener("click", () => {
+      inner.scrollBy(-350, 0);
+    });
+
+    rightArrow.addEventListener("click", () => {
+      inner.scrollBy(350, 0);
+    });
   }
 }
--- 7-module/4-task/index.js
+++ 7-module/4-task/index.js
@@ -1,4 +1,301 @@
+// // // export default class StepSlider {
+// // //   constructor({ steps, value = 0 }) {
+// // //   }
+// // // }
+// // // index.js
+// // export default class StepSlider {
+// //   constructor({ steps, value = 0 }) {
+// //     this.steps = steps;
+// //     this.value = value;
+// //     this.render();
+
+// //     // Начальные координаты для обработчиков Drag-and-Drop
+// //     this.shiftX = 0;
+// //     this.thumb = this.elem.querySelector(".slider__thumb");
+// //     this.progress = this.elem.querySelector(".slider__progress");
+// //     this.stepsElements = this.elem.querySelectorAll(".slider__steps span");
+
+// //     this.thumb.addEventListener("pointerdown", this.onPointerDown);
+// //     this.elem.addEventListener("click", this.onSliderClick);
+// //   }
+
+// //   render() {
+// //     // Создание DOM-элементов слайдера
+// //     this.elem = document.createElement("div");
+// //     this.elem.classList.add("slider");
+
+// //     // Добавление DOM-элементов в корневой элемент слайдера
+// //     this.elem.innerHTML = `
+// //       <div class="slider__thumb" style="left: ${
+// //         this.value * (100 / (this.steps - 1))
+// //       }%;">
+// //         <span class="slider__value">${this.value}</span>
+// //       </div>
+// //       <div class="slider__progress" style="width: ${
+// //         this.value * (100 / (this.steps - 1))
+// //       }%;"></div>
+// //       <div class="slider__steps">
+// //         ${Array.from({ length: this.steps })
+// //           .map(
+// //             (_, index) =>
+// //               `<span ${
+// //                 index === 0 ? 'class="slider__step-active"' : ""
+// //               }></span>`
+// //           )
+// //           .join("")}
+// //       </div>
+// //     `;
+
+// //     // Отключение встроенного Drag-and-Drop
+// //     this.thumb.ondragstart = () => false;
+
+// //     document.getElementById("slider").appendChild(this.elem);
+// //   }
+
+// //   onPointerDown = (event) => {
+// //     event.preventDefault();
+
+// //     this.elem.classList.add("slider_dragging");
+
+// //     // Сохранение начальных координат
+// //     this.shiftX = event.clientX - this.thumb.getBoundingClientRect().left;
+
+// //     // Добавление обработчиков
+// //     document.addEventListener("pointermove", this.onPointerMove);
+// //     document.addEventListener("pointerup", this.onPointerUp);
+// //   };
+
+// //   onPointerMove = (event) => {
+// //     let newLeft =
+// //       event.clientX - this.elem.getBoundingClientRect().left - this.shiftX;
+// //     let leftRelative = newLeft / this.elem.offsetWidth;
+
+// //     if (leftRelative < 0) {
+// //       leftRelative = 0;
+// //     }
+
+// //     if (leftRelative > 1) {
+// //       leftRelative = 1;
+// //     }
+
+// //     let leftPercents = leftRelative * 100;
+
+// //     this.thumb.style.left = `${leftPercents}%`;
+// //     this.progress.style.width = `${leftPercents}%`;
+
+// //     let approximateValue = leftRelative * (this.steps - 1);
+// //     this.value = Math.round(approximateValue);
+
+// //     this.stepsElements.forEach((step, index) => {
+// //       index === this.value
+// //         ? step.classList.add("slider__step-active")
+// //         : step.classList.remove("slider__step-active");
+// //     });
+// //   };
+
+// //   onPointerUp = () => {
+// //     this.elem.classList.remove("slider_dragging");
+
+// //     // Удаление обработчиков
+// //     document.removeEventListener("pointermove", this.onPointerMove);
+// //     document.removeEventListener("pointerup", this.onPointerUp);
+
+// //     // Генерация события изменения значения
+// //     this.elem.dispatchEvent(
+// //       new CustomEvent("slider-change", {
+// //         detail: this.value,
+// //         bubbles: true,
+// //       })
+// //     );
+// //   };
+
+// //   onSliderClick = (event) => {
+// //     let newLeft = event.clientX - this.elem.getBoundingClientRect().left;
+// //     let leftRelative = newLeft / this.elem.offsetWidth;
+
+// //     if (leftRelative < 0) {
+// //       leftRelative = 0;
+// //     }
+
+// //     if (leftRelative > 1) {
+// //       leftRelative = 1;
+// //     }
+
+// //     let leftPercents = leftRelative * 100;
+
+// //     this.thumb.style.left = `${leftPercents}%`;
+// //     this.progress.style.width = `${leftPercents}%`;
+
+// //     let approximateValue = leftRelative * (this.steps - 1);
+// //     this.value = Math.round(approximateValue);
+
+// //     this.stepsElements.forEach((step, index) => {
+// //       index === this.value
+// //         ? step.classList.add("slider__step-active")
+// //         : step.classList.remove("slider__step-active");
+// //     });
+
+// //     // Генерация события изменения значения
+// //     this.elem.dispatchEvent(
+// //       new CustomEvent("slider-change", {
+// //         detail: this.value,
+// //         bubbles: true,
+// //       })
+// //     );
+// //   };
+// // }
+
+// // // Использование
+// // const slider = new StepSlider({ steps: 5, value: 2 });
+// // slider.elem.addEventListener("slider-change", (event) => {
+// //   console.log("Slider value changed:", event.detail);
+// // });
+
+index.js;
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.steps = steps;
+    this.value = value;
+    this.render();
+
+    // Начальные координаты для обработчиков Drag-and-Drop
+    this.shiftX = 0;
+    this.thumb = this.elem.querySelector(".slider__thumb");
+    this.progress = this.elem.querySelector(".slider__progress");
+    this.stepsElements = this.elem.querySelectorAll(".slider__steps span");
+
+    this.thumb.addEventListener("pointerdown", this.onPointerDown);
+    this.elem.addEventListener("click", this.onSliderClick);
   }
+
+  render() {
+    // Создание DOM-элементов слайдера
+    this.elem = document.createElement("div");
+    this.elem.classList.add("slider");
+
+    // Добавление DOM-элементов в корневой элемент слайдера
+    this.elem.innerHTML = `
+      <div class="slider__thumb" style="left: ${
+        this.value * (100 / (this.steps - 1))
+      }%;">
+        <span class="slider__value">${this.value}</span>
+      </div>
+      <div class="slider__progress" style="width: ${
+        this.value * (100 / (this.steps - 1))
+      }%;"></div>
+      <div class="slider__steps">
+        ${Array.from({ length: this.steps })
+          .map(
+            (_, index) =>
+              `<span ${
+                index === 0 ? 'class="slider__step-active"' : ""
+              }></span>`
+          )
+          .join("")}
+      </div>
+    `;
+
+    // Отключение встроенного Drag-and-Drop
+    this.thumb.ondragstart = () => false;
+
+    document.getElementById("slider").appendChild(this.elem);
+  }
+
+  onPointerDown = (event) => {
+    event.preventDefault();
+
+    this.elem.classList.add("slider_dragging");
+
+    // Сохранение начальных координат
+    this.shiftX = event.clientX - this.thumb.getBoundingClientRect().left;
+
+    // Добавление обработчиков
+    document.addEventListener("pointermove", this.onPointerMove);
+    document.addEventListener("pointerup", this.onPointerUp);
+  };
+
+  onPointerMove = (event) => {
+    let newLeft =
+      event.clientX - this.elem.getBoundingClientRect().left - this.shiftX;
+    let leftRelative = newLeft / this.elem.offsetWidth;
+
+    if (leftRelative < 0) {
+      leftRelative = 0;
+    }
+
+    if (leftRelative > 1) {
+      leftRelative = 1;
+    }
+
+    let leftPercents = leftRelative * 100;
+
+    this.thumb.style.left = `${leftPercents}%`;
+    this.progress.style.width = `${leftPercents}%`;
+
+    let approximateValue = leftRelative * (this.steps - 1);
+    this.value = Math.round(approximateValue);
+
+    this.stepsElements.forEach((step, index) => {
+      index === this.value
+        ? step.classList.add("slider__step-active")
+        : step.classList.remove("slider__step-active");
+    });
+  };
+
+  onPointerUp = () => {
+    this.elem.classList.remove("slider_dragging");
+
+    // Удаление обработчиков
+    document.removeEventListener("pointermove", this.onPointerMove);
+    document.removeEventListener("pointerup", this.onPointerUp);
+
+    // Генерация события изменения значения
+    this.elem.dispatchEvent(
+      new CustomEvent("slider-change", {
+        detail: this.value,
+        bubbles: true,
+      })
+    );
+  };
+
+  onSliderClick = (event) => {
+    let newLeft = event.clientX - this.elem.getBoundingClientRect().left;
+    let leftRelative = newLeft / this.elem.offsetWidth;
+
+    if (leftRelative < 0) {
+      leftRelative = 0;
+    }
+
+    if (leftRelative > 1) {
+      leftRelative = 1;
+    }
+
+    let leftPercents = leftRelative * 100;
+
+    this.thumb.style.left = `${leftPercents}%`;
+    this.progress.style.width = `${leftPercents}%`;
+
+    let approximateValue = leftRelative * (this.steps - 1);
+    this.value = Math.round(approximateValue);
+
+    this.stepsElements.forEach((step, index) => {
+      index === this.value
+        ? step.classList.add("slider__step-active")
+        : step.classList.remove("slider__step-active");
+    });
+
+    // Генерация события изменения значения
+    this.elem.dispatchEvent(
+      new CustomEvent("slider-change", {
+        detail: this.value,
+        bubbles: true,
+      })
+    );
+  };
 }
+
+// Использование
+const slider = new StepSlider({ steps: 5, value: 2 });
+slider.elem.addEventListener("slider-change", (event) => {
+  console.log("Slider value changed:", event.detail);
+});
