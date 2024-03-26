62adb67df5880732648eb4dca4b8967106c57197
Maksim Levanovich <maxlevanovich@gmail.com>
2024-03-26 14:22:41 -0500

    8-module

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -1,4 +1,4 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class CartIcon {
   constructor() {
@@ -13,32 +13,107 @@ export default class CartIcon {
 
   update(cart) {
     if (!cart.isEmpty()) {
-      this.elem.classList.add('cart-icon_visible');
+      this.elem.classList.add("cart-icon_visible");
 
       this.elem.innerHTML = `
         <div class="cart-icon__inner">
           <span class="cart-icon__count">${cart.getTotalCount()}</span>
-          <span class="cart-icon__price">€${cart.getTotalPrice().toFixed(2)}</span>
+          <span class="cart-icon__price">€${cart
+            .getTotalPrice()
+            .toFixed(2)}</span>
         </div>`;
 
       this.updatePosition();
 
-      this.elem.classList.add('shake');
-      this.elem.addEventListener('transitionend', () => {
-        this.elem.classList.remove('shake');
-      }, {once: true});
-
+      this.elem.classList.add("shake");
+      this.elem.addEventListener(
+        "transitionend",
+        () => {
+          this.elem.classList.remove("shake");
+        },
+        { once: true }
+      );
     } else {
-      this.elem.classList.remove('cart-icon_visible');
+      this.elem.classList.remove("cart-icon_visible");
     }
   }
 
   addEventListeners() {
-    document.addEventListener('scroll', () => this.updatePosition());
-    window.addEventListener('resize', () => this.updatePosition());
+    document.addEventListener("scroll", () => this.updatePosition());
+    window.addEventListener("resize", () => this.updatePosition());
   }
 
   updatePosition() {
-    // ваш код ...
+    // Проверяем ширину окна браузера
+    const isMobile = document.documentElement.clientWidth <= 767;
+
+    // Если ширина окна меньше или равна 767px, обнуляем стили и выходим из метода
+    if (isMobile) {
+      Object.assign(this.elem.style, {
+        position: "",
+        top: "",
+        left: "",
+        zIndex: "",
+      });
+      return;
+    }
+
+    if (!this.initialTopCoord) {
+      this.initialTopCoord =
+        this.elem.getBoundingClientRect().top + window.pageYOffset;
+    }
+
+    const initialTopCoord = this.initialTopCoord;
+
+    // Получаем элемент .container
+    const container = document.querySelector(".container");
+
+    // Проверяем, существует ли элемент .container
+    if (container) {
+      // Вычисляем отступ по горизонтали
+      const containerRight = container.getBoundingClientRect().right + 20;
+      const windowWidth = document.documentElement.clientWidth;
+      const iconWidth = this.elem.offsetWidth;
+      const minLeftIndent = windowWidth - iconWidth - 10;
+      let leftIndent = Math.min(containerRight, minLeftIndent) + "px";
+
+      // Применяем фиксированное позиционирование, если пользователь прокрутил до конца страницы
+      if (window.pageYOffset > initialTopCoord) {
+        Object.assign(this.elem.style, {
+          position: "fixed",
+          top: "50px",
+          zIndex: 1000,
+          right: "20px",
+          left: leftIndent,
+        });
+      } else {
+        // Возвращаем обычное позиционирование
+        Object.assign(this.elem.style, {
+          position: "",
+          top: "",
+          left: "",
+          zIndex: "",
+        });
+      }
+    } else {
+      // Применяем фиксированное позиционирование с учетом отступа от правого края окна
+      if (window.pageYOffset > initialTopCoord) {
+        Object.assign(this.elem.style, {
+          position: "fixed",
+          top: "50px",
+          zIndex: 1000,
+          right: "10px",
+          left: "10px", // Отступ от левого края окна по умолчанию
+        });
+      } else {
+        // Возвращаем обычное позиционирование
+        Object.assign(this.elem.style, {
+          position: "",
+          top: "",
+          left: "",
+          zIndex: "",
+        });
+      }
+    }
   }
 }
